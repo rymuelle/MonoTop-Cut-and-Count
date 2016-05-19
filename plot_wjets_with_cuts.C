724be8f0a5e3f3738e2e9689ffc11f4b8ab4d6bc
Ryan Mueller <33radar@gmail.com>
2016-05-20 01:08:18 +0200

    this time add other regions

--- plot_wjets_with_cuts.C
+++ plot_wjets_with_cuts.C
@@ -16,6 +16,15 @@ void plot_wjets_with_cuts(){
     m.Signal_cutflow.push_back(cut_flow_count("delta_phi_muon_bjet_signal"));
     m.Signal_cutflow.push_back(cut_flow_count("met_signal"));
 
+    m.WJet_cutflow.push_back(cut_flow_count("all events WJet region wjet"));
+    m.WJet_cutflow.push_back(cut_flow_count("high_pt_not_bjet_wjet"));
+    m.WJet_cutflow.push_back(cut_flow_count("other_jet_WJet"));
+
+    m.TTBar_cutflow.push_back(cut_flow_count("all events TTbar region wjet"));
+    m.TTBar_cutflow.push_back(cut_flow_count("two_bjets wjet"));
+    m.TTBar_cutflow.push_back(cut_flow_count("leading jet pt wjet"));
+
+
     //initilize plots
     m.PreCut_analysis_plots = new analysis_plots("WJET_precut");
     m.Signal_analysis_plots = new analysis_plots("WJET_Signal");
@@ -27,6 +36,8 @@ void plot_wjets_with_cuts(){
     
     m.print_cutflow(m.Precut_cutflow);
     m.print_cutflow(m.Signal_cutflow);
+    m.print_cutflow(m.WJet_cutflow);
+    m.print_cutflow(m.TTBar_cutflow);
 
     m.Draw_plots(m.PreCut_analysis_plots);
     m.Draw_plots(m.Signal_analysis_plots);
